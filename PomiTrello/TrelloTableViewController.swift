a6ad7589acd104cbd63cba0a4503d5ab71e7de22
Alexander Guschin <alexander.ragon@gmail.com>
2015-09-19 21:48:39 +0300

    Initial commit

---
+++ PomiTrello/TrelloTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TrelloTableViewController.swift
+//  PomiTrello
+//
+//  Created by Александр on 19.09.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class TrelloTableViewController: UITableViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
