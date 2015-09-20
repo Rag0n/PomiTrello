a40cec30fb18e4632b9802b5b7badbb064f73bcf
Alexander Guschin <alexander.ragon@gmail.com>
2015-09-20 18:02:16 +0300

    Adding Board model

---
+++ PomiTrello/Board.swift
@@ -0,0 +1,15 @@
+//
+//  Board.swift
+//  PomiTrello
+//
+//  Created by Александр on 20.09.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+import Foundation
+
+class Board {
+    var name: String!
+    var closed: Bool!
+    var url: NSURL!
+}
