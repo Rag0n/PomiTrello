49ad1441df67d9c36bd9880ec3a870a69d9e8df4
Alexander Guschin <alexander.ragon@gmail.com>
2015-09-29 23:25:16 +0300

    Добавлен WelcomeViewController

---
+++ PomiTrello/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  PomiTrello
+//
+//  Created by Александр on 29.09.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
