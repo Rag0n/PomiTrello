ef2657f7e9aa51e0d41a4102f51072a92c607004
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-05 12:54:43 +0300

    Creating managed object context

--- PomiTrello/AppDelegate.swift
+++ PomiTrello/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let managedObjectContext = createBoardsMainContext()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         let defaults = NSUserDefaults.standardUserDefaults()
