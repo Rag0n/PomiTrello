6c4283c2c69245df6b97726dc1e8ceadd989cddc
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-06 16:49:13 +0300

    Impelemting FetchedResultsDataProvider: init, objectAtIndexPath, numberOfItemsInSection

--- PomiTrello/FetchedResultsDataProvider.swift
+++ PomiTrello/FetchedResultsDataProvider.swift
@@ -8,6 +8,39 @@
 
 import CoreData
 
-class FetchedResultsDataProvider<Delegate: DataProviderDelegate>: NSObject, NSFetchedResultsControllerDelegate, DataProvider {
-    <#properties and methods#>
+// know about all underlying data changes
+class FetchedResultsDataProvider<Delegate: DataProviderDelegate> : NSObject, NSFetchedResultsControllerDelegate, DataProvider {
+    
+    typealias Object = Delegate.Object
+    
+    init(fetchedResultController: NSFetchedResultsController, delegate: Delegate) {
+        self.fetchedResultsController = fetchedResultController
+        self.delegate = delegate
+        super.init()
+        fetchedResultsController.delegate = self
+        try! fetchedResultsController.performFetch()
+    }
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        
+    }
+    
+    
+    // MARK: - DataProvider Protocol
+    func objectAtIndexPath(indexPath: NSIndexPath) -> Object {
+        guard let result = fetchedResultsController.objectAtIndexPath(indexPath) as? Object else {
+            fatalError("Unexpected object at \(indexPath)")
+        }
+        return result
+    }
+    
+    func numberOfItemsInSection(section: Int) -> Int {
+        guard let sec = fetchedResultsController.sections?[section] else { return 0 }
+        return sec.numberOfObjects
+    }
+    
+    // MARK: - Private
+    private let fetchedResultsController: NSFetchedResultsController
+    private weak var delegate: Delegate!
 }
