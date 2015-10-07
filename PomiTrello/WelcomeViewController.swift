9a69c80a6b74802006bfe0971ece10cbed52058c
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-07 23:05:55 +0300

    Sending managedObjectContext to loginViewController

--- PomiTrello/WelcomeViewController.swift
+++ PomiTrello/WelcomeViewController.swift
@@ -18,7 +18,12 @@ class WelcomeViewController: UIViewController, ManagedObjectContextSettable {
         UIView.animateWithDuration(1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: { () -> Void in
             self.loginButton.bounds.size.width += 80
         }, completion: nil)
+        
         let lvc = storyboard!.instantiateViewControllerWithIdentifier(StoryBoard.loginViewControllerIdentifier)
+        guard let vc = lvc as? ManagedObjectContextSettable else {
+            fatalError("Wrong view controller type(VC should conform to ManagedObjectContextSettable protocol)")
+        }
+        vc.managedObjectContext = managedObjectContext
         presentViewController(lvc, animated: true, completion: nil)
     }
 }
