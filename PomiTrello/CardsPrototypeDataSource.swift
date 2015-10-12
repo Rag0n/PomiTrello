de2365432865b381f73b7b0803670859e3dffe0c
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-12 23:58:38 +0300

    Implementing required data source methods

--- PomiTrello/CardsPrototypeDataSource.swift
+++ PomiTrello/CardsPrototypeDataSource.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import UIKit
+
+class CardsPrototypeDataSource: NSObject, UITableViewDataSource {
+    
+    var items: AnyObject
+    var cellIdentifier: String
+    var configureCell: () -> Void
+    
+    init(withItems: AnyObject, cellIdentifier: String, configureCell: () -> Void) {
+        self.configureCell = configureCell
+        self.items = withItems
+        self.cellIdentifier = cellIdentifier
+        super.init()
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+//        configureCell(cell, 
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+}
