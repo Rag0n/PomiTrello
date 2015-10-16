d71cf002c53f505fa4509bc9d4a4f537f13cbb29
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-16 15:15:17 +0300

    Создана модель данных pomodoro Добавлен Card entity

--- PomiTrello/Card.swift
+++ PomiTrello/Card.swift
@@ -9,23 +9,17 @@
 import Foundation
 import CoreData
 
-public final class Card {
-//    @NSManaged private(set) var position: Int32
-//    @NSManaged private(set) var id: String!
-//    @NSManaged private(set) var name: String!
-    
-    var id: String!
-    var name: String!
-    
-//    @NSManaged private(set) var list: List!
+public final class Card: ManagedObject {
+    @NSManaged private(set) var id: String?
+    @NSManaged private(set) var name: String!
 }
 
-extension Card: ManagedObjectType {
-    public static var entityName: String {
-        return "Card"
-    }
-    
-    public static var defaultSortDescriptors: [NSSortDescriptor] {
-        return [NSSortDescriptor(key: "position", ascending: false)]
-    }
-}
+//extension Card: ManagedObjectType {
+//    public static var entityName: String {
+//        return "Card"
+//    }
+//    
+//    public static var defaultSortDescriptors: [NSSortDescriptor] {
+//        return [NSSortDescriptor(key: "position", ascending: false)]
+//    }
+//}
