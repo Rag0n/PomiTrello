021430e02a9fe75a8c56a893648c71d708699155
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-20 14:20:11 +0300

    Добавлен NSURL documentsURL Обновлены комментарии

--- PomiTrello/CardStack.swift
+++ PomiTrello/CardStack.swift
@@ -10,17 +10,35 @@ import CoreData
 
 private let StoreURL = NSURL.documentsURL.URLByAppendingPathComponent("Card.card")
 
+
 public func createCardMainContext() -> NSManagedObjectContext {
+    // получаем bundle(будет работать, если перенесем в другой модуль)
     let bundle = NSBundle(forClass: Card.self)
+    
+    // загружаем объектную модель
     guard let model = NSManagedObjectModel.mergedModelFromBundles([bundle]) else {
         fatalError("model not found")
     }
     
+    // создаем persistent store coordinator для нашей модели и добавляем persistent store
     let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
-    try psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: StoreURL, options: nil)
+    try! psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: StoreURL, options: nil)
     
+    // привязываем контекст к main thread
+    // т.о можем безопасно обращаться к контексту в UI коде
     let context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
     context.persistentStoreCoordinator = psc
     
     return context
+}
+
+extension NSURL {
+    // возвращает url директории документов
+    static var documentsURL: NSURL {
+        let fileManager = NSFileManager.defaultManager()
+        
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        
+        return urls[0]
+    }
 }
