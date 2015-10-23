f34ec0c5922b415ce9dfd70c07e01d9fe3aa44e9
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-23 08:25:55 +0300

    Изменен контекст

--- PomiTrello/AppDelegate.swift
+++ PomiTrello/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let managedObjectContext = createCardMainContext()
+    let managedObjectContext = createPomodoroMainContext()
     
 //    let managedObjectContext = createBoardsMainContext()
 
