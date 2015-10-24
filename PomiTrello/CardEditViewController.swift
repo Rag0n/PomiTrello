3eb9f717398273e9ce6f9e74075a82667e845332
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-24 07:36:06 +0300

    Добавлено окно для добавления новой задачи

---
+++ PomiTrello/CardEditViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CardEditViewController.swift
+//  PomiTrello
+//
+//  Created by Александр on 24.10.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class CardEditViewController: UIViewController {
+
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
