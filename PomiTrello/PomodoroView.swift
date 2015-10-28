82ec872554c2f127a9a39395543851400bcb2cd7
Alexander Guschin <alexander.ragon@gmail.com>
2015-10-28 06:15:43 +0300

    Добавлены вычисляемые свойства радиуса и центра

--- PomiTrello/PomodoroView.swift
+++ PomiTrello/PomodoroView.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 class PomodoroView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    
+    var scale: CGFloat = 0.8 { didSet { setNeedsDisplay() } }
+    
+    
+    private var pomodoroCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
     }
-    */
+    
+    private var pomodoroRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+
+    override func drawRect(rect: CGRect) {
+        let path = UIBezierPath()
+        path.addArcWithCenter(pomodoroCenter, radius: pomodoroRadius, startAngle: 0, endAngle: CGFloat(2 * M_PI), clockwise: true)
+        path.closePath()
+        UIColor.redColor().setStroke()
+        path.lineWidth = 3.0
+        
+        path.stroke()
+    }
+
 
 }
